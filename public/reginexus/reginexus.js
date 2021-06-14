dd7e234822357fe6cba8e430e689444c9288abf9
Destrian <manucide@live.com>
2021-06-15 01:57:55 +0200

    copy to clipboard button

--- public/reginexus/reginexus.js
+++ public/reginexus/reginexus.js
@@ -6,10 +6,11 @@ const { ethereum } = window;
 //Buttons
 const connect = document.getElementById('connect');
 const sign = document.getElementById('sign');
+const copy = document.getElementById('copy');
 //Information display
 const account = document.getElementById('account');
 const command = document.getElementById('command');
-
+const copiedmsg = document.getElementById('copiedmsg');
 const uuid = document.getElementById('uuid').textContent;
 
 const onClickConnect = async () => {
@@ -20,6 +21,7 @@ const onClickConnect = async () => {
     }
     const accounts = await ethereum.request({ method: 'eth_accounts' });
     account.textContent = accounts[0];
+    sign.disabled =false;
   };
 
 const onClickSign = async () => {
@@ -37,9 +39,17 @@ const onClickSign = async () => {
     ethereum.sendAsync({method, params, from},
       function (err, response) {
         command.textContent = "/verify " + accounts[0]+" "+response.result;
+        command.value = "/verify " + accounts[0]+" "+response.result;
+        copy.disabled = false;
         console.log('Response json: ' + JSON.stringify(response))
       }
     );
+  };
+  const onClickCopy = async () => {
+    command.select();
+    command.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    copiedmsg.textContent = "Copied to clipboard!"
   };
 
 sign.addEventListener('click', function (e) {
@@ -49,6 +59,8 @@ sign.addEventListener('click', function (e) {
 connect.addEventListener('click', function(e) {
     onClickConnect();
 });
-
+copy.addEventListener('click', function(e) {
+  onClickCopy();
+});
 
 
